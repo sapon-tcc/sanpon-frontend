611b1252729590a3954612e811b6c82d4e2e3861
SantanaVictoria <victoriasantana103@gmail.com>
2023-10-06 21:44:13 -0300

    tela cadastro e tela inicial estão sendo feitas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './pages/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
 import { HeaderComponent } from './components/header/header.component';
+import { CadastroComponent } from './pages/cadastro/cadastro.component';
 // import { HeaderComponent } from './pages/header/header.component';
 
 @NgModule({
@@ -14,6 +15,7 @@ import { HeaderComponent } from './components/header/header.component';
     LoginComponent,
     HomeComponent,
     HeaderComponent,
+    CadastroComponent,
   ],
   imports: [
     BrowserModule,
