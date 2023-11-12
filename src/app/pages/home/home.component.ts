372980933f35b1f13829f0360950e24a8b180828
DanielNery <danielpontesnery@gmail.com>
2023-11-11 21:20:21 -0300

    feat: Adicionando nova seção de conteúdo header.

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -15,6 +15,11 @@ SwiperCore.use([Navigation, Pagination]);
   encapsulation: ViewEncapsulation.None
 })
 export class HomeComponent implements OnInit {
+  foto1: string = 'assets/images/pexels-john-ray-ebora-4581325 1.png';
+  foto2: string = 'assets/images/pexels-cottonbro-6055263 1.png';
+  foto3: string = 'assets/images/pexels-brett-jordan-5417837 1.png';
+  foto4: string = 'assets/images/pexels-pixabay-159866 1.png';
+  foto5: string = 'assets/images/pexels-tim-samuel-5838911 1.png';
   gatinhoIcone: string = 'assets/images/cat-icon.png';
   books: [] = [];
   currentIndex = 0;
